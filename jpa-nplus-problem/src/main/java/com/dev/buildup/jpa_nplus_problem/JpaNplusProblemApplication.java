235ec4809900152b2ca66b5ed87a10390600749d
devanya20220806-1 <devanya20220806@gmail.com>
2026-04-23 21:15:40 +0530

    added new project

---
+++ jpa-nplus-problem/src/main/java/com/dev/buildup/jpa_nplus_problem/JpaNplusProblemApplication.java
@@ -0,0 +1,13 @@
+package com.dev.buildup.jpa_nplus_problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaNplusProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaNplusProblemApplication.class, args);
+	}
+
+}
---
+++ jpa-nplus-problem/src/test/java/com/dev/buildup/jpa_nplus_problem/JpaNplusProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.buildup.jpa_nplus_problem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaNplusProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
